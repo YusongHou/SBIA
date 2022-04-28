84abb2823964118efee6e7504d23c0ddb787eadf
houyusong <403683723>
2022-04-28 16:22:11 +0800

    feat(*):项目初始化

---
+++ src/test/java/com/example/SBIA/SbiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
